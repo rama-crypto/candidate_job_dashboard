6f6b4e413c22753f8b76522e328f682e84150ac4
unknown <ramangarg325@gmail.com>
2022-10-15 17:58:02 +0530

    heroku backend

--- constants/api-config.js
+++ constants/api-config.js
@@ -1,8 +1,6 @@
 const BASE_URLS = {
     local: 'http://127.0.0.1:8000/',
-    staging: 'https://testapi.novaweb.in/',
-    prod: 'https://api.novaweb.in/',
+    prod: 'https://job-portl-bckend.herokuapp.com/',
 }
 
-export const API_URL = BASE_URLS.local
-    // process.env.BACKEND === 'production' ? BASE_URLS.prod : BASE_URLS.staging
+export const API_URL = BASE_URLS.prod
